643e2354932fff803443039096db75d8684d5523
Gusarich <danielsedovzzz@gmail.com>
2024-03-30 23:40:37 +0300

    fix

--- scripts/deployTask.ts
+++ scripts/deployTask.ts
@@ -1,11 +1,19 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { Task } from '../wrappers/Task';
 import { compile, NetworkProvider } from '@ton/blueprint';
+import { JettonMinter } from '../wrappers/JettonMinter';
 
 export async function run(provider: NetworkProvider) {
-    const task = provider.open(Task.createFromConfig({}, await compile('Task')));
+    const task = provider.open(Task.createFromConfig({
+        admin: Address.parse('EQCJ6QtYriuerrHkFfIbAurua_ODPyj36FeyQ6NdWSHIyetp'),
+        publicKey: Buffer.from('2222adf595c4c335dfb7c26c5d9587382741443bafa789958d93d81de1c9caa8', 'hex'),
+        helperCode: await compile('Helper'),
+        reward: toNano('0.2')
+    }, await compile('Task')));
 
-    await task.sendDeploy(provider.sender(), toNano('0.05'));
+    const jettonMinter = provider.open(JettonMinter.createFromAddress(Address.parse('EQCJbp0kBpPwPoBG-U5C-cWfP_jnksvotGfArPF50Q9Qiv9h')))
+
+    await task.sendDeploy(provider.sender(), toNano('0.05'), {jettonWallet: await jettonMinter.getWalletAddressOf(task.address)});
 
     await provider.waitForDeploy(task.address);
 
